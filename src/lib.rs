213e5449285749a747dd420389a161b64d188bc5
Faizaan <aulisius7@gmail.com>
2016-05-12 17:29:52 +0530

    Initial

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 struct Config {
-    add_vendor_prefix: bool
+    add_vendor_prefix: bool,
 }
 
 #[derive(Debug)]
@@ -16,55 +16,55 @@ enum Color {
 
 #[derive(Debug)]
 struct CSSElement {
-       name : String,
-       properties : HashMap<String, String>
+    name: String,
+    properties: HashMap<String, String>,
 }
 
 #[derive(Debug)]
 struct VerbalCSS {
-    config : Config,
-    curr_elem : Option<CSSElement>
+    config: Config,
+    curr_elem: Option<CSSElement>,
 }
 
 impl VerbalCSS {
     fn new(c: Config) -> Self {
         VerbalCSS {
-            config : c,
-            curr_elem : None
+            config: c,
+            curr_elem: None,
         }
     }
-    
+
     fn new_elem(mut self, elem: String) -> Self {
         self.curr_elem = Some(CSSElement {
-                            name : elem,
-                            properties : HashMap::new()
+            name: elem,
+            properties: HashMap::new(),
         });
         self
     }
-    
-    fn with_color(mut self, color : Color) -> Self {
-    
+
+    fn with_color(mut self, color: Color) -> Self {
+
         let color_str = match color {
-                    Color::Hex(s) => format!("#{}", s),
-                    Color::Rgb(r, g, b) => format!("rgb({},{},{})", r, g, b),
-                    Color::Rgba(r, g, b, a) => format!("rgba({},{},{},{})", r, g, b, a),
-                    Color::Hsl(h, s, l) => format!("hsl({},{},{})", h, s, l),
-                    Color::Hsla(h, s, l, a) => format!("hsla({},{},{},{})", h, s, l, a)
-                };
-    
+            Color::Hex(s) => format!("#{}", s),
+            Color::Rgb(r, g, b) => format!("rgb({},{},{})", r, g, b),
+            Color::Rgba(r, g, b, a) => format!("rgba({},{},{},{})", r, g, b, a),
+            Color::Hsl(h, s, l) => format!("hsl({},{},{})", h, s, l),
+            Color::Hsla(h, s, l, a) => format!("hsla({},{},{},{})", h, s, l, a),
+        };
+
         if let Some(ref mut elem) = self.curr_elem {
             elem.properties.insert("color".to_string(), color_str);
         } else {
             println!("No element to add color to! Make an element first!");
         }
-        
+
         self
     }
-    
+
     fn make(mut self) -> String {
         if let Some(ref mut elem) = self.curr_elem {
             let mut tmp = String::new();
-            
+
             for (k, v) in elem.properties.iter() {
                 tmp.push_str(&format!("{}:{};\n", k, v));
             }
@@ -78,12 +78,10 @@ impl VerbalCSS {
 }
 
 fn main() {
-    let mut verbal_css = VerbalCSS::new(Config {
-                    add_vendor_prefix : false
-                });
-        
-    println!("{:?}", verbal_css.new_elem("li".to_string())
-                        .with_color(Color::Hex("fff".to_string()))
-                        .make()
-                      );
+    let mut verbal_css = VerbalCSS::new(Config { add_vendor_prefix: false });
+
+    println!("{:?}",
+             verbal_css.new_elem("li".to_string())
+                       .with_color(Color::Hex("fff".to_string()))
+                       .make());
 }
